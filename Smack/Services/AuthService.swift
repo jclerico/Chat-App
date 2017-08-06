12431b860ca132d94695a79a8023ef906c8787d9
Jeremy Clerico <blops2hd@gmail.com>
2017-08-06 15:50:18 +0100

    Created web request for registering user

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,81 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Jeremy Clerico on 06/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    //Most simple way of saving data in your app. Dont use for any heavy duty data eg images. Only use for string, booleans etc. Also not very secure so dont save passwords etc here.
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            //Look into user defaults and see if value exists
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    //^Able to access these three variables anywhere and also be reading and writing to and from our user defaults. Variables will be persisted across app launches.
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}

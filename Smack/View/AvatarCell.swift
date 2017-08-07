d3a673fde8528a7fdd7da7bb020666ee72d01b41
Jeremy Clerico <blops2hd@gmail.com>
2017-08-07 11:08:46 +0100

    Connected Outlets/Actions and setup Collection View

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,28 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Jeremy Clerico on 07/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    
+}

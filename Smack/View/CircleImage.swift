7874a91479d18401087e55291e5fa52eea7baee1
Jeremy Clerico <blops2hd@gmail.com>
2017-08-07 12:20:07 +0100

    Completed Avatar Picker+BGColour and Finished CreateAccountVC

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,29 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Jeremy Clerico on 07/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class CircleImage: UIImageView {
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+        
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+}

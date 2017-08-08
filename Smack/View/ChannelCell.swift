532b090eac8cd48f955291d1458b837501c35805
Jeremy Clerico <blops2hd@gmail.com>
2017-08-08 15:58:49 +0100

    Unread channels now show bold&larger size until messages are read.

--- Smack/View/ChannelCell.swift
+++ Smack/View/ChannelCell.swift
@@ -35,6 +35,13 @@ class ChannelCell: UITableViewCell {
     func configureCell(channel: Channel) {
         let title = channel.channelTitle ?? "" //If no value return ""
         channelName.text = "#\(title)"
+        channelName.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        
+        for id in MessageService.instance.unreadChannels {
+            if id == channel.id {
+                channelName.font = UIFont(name: "HelveticaNeue-Bold", size: 22)
+            }
+        }
     }
     
     
